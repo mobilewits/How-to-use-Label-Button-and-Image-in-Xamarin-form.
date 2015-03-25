79964756411446ba9c86b77c92a6a0dceb3ab25e
Harry <Harry@Harrys-Mac-Pro-2.local>
2015-03-25 11:18:14 -0400

    How to use Label,Button and Image in Xamarin form.

---
+++ StdControlsXamarinForm/StdControlsXamarinForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace StdControlsXamarinForm
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			Label lbl_name = new Label {
+				Text = "This is Label",
+				//VerticalOptions=
+			};
+			Image img = new Image {
+				Source="Default.png",
+				HeightRequest=200,
+				WidthRequest=200
+			};
+			Button btn_click = new Button {
+				Text = "Click here"
+			};
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						lbl_name, img,btn_click
+					}
+				}
+			};
+			btn_click.Clicked += (sender, e) => {
+				lbl_name.Text="Button Clicked";
+			};
+		
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
